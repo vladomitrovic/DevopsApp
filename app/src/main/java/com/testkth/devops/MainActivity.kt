ec2c68ddfcfb76083ed4c1d9f5e621b033690d18
vladomitrovic <vlado.mitrovic@outlook.com>
2021-04-19 17:47:13 +0200

    Modification

--- app/src/main/java/com/testkth/devops/MainActivity.kt
+++ app/src/main/java/com/testkth/devops/MainActivity.kt
@@ -8,7 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
-
+    //Added modification
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
